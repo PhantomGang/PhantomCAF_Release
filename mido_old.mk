c401726faab94e9ab0322d65c46c2154f21513dd
Alif Fathur <53365858+herobuxx@users.noreply.github.com>
2020-05-27 19:40:00 +0700

    Create mido_old.mk

---
+++ mido_old.mk
@@ -0,0 +1,29 @@
+# PhantomCAF For Redmi Note 4/4X - Mido
+
+Release Version : Arcadia - 1.0
+
+Linux Kernel Version : 3.18.140
+
+Android Support : 10 / Q
+
+
+[Download](https://semawur.com/fv4c)
+
+## Changelogs
+
+```
+## Arcadia - 1.0
+- New version of ViviaKernel
+- Added Darkness corvernor
+- Added fsync toggling support
+- Built with SDClang optimized for Snapdragon
+- UV to reduce heat
+- Optimze nohz Idle enter
+- Optimize Prima wifi drive with O3
+- Kill Unecessary Google apps in background
+- Cortex a53 optimization
+- Align slab caches to L1 cache line
+- enable usb fastcharge
+- Misc Phantom tweakss on WiFi, CPU, and GPU
+- So many other changes added
+```
